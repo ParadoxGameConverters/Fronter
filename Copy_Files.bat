ac7a85e5c6457291b66ec443c7f225af8dec7cab
Ivo Elezovic <ivo@elezovic.su>
2020-04-28 11:28:18 +0200

    update commons

--- Copy_Files.bat
+++ Copy_Files.bat
@@ -1,8 +1,8 @@
 echo on
 rem Copy required DLLs
-copy "wx\3.1.3\lib\vc14x_x64_dll\wxbase313u_vc14x_x64.dll" "Release\"
-copy "wx\3.1.3\lib\vc14x_x64_dll\wxmsw313u_core_vc14x_x64.dll" "Release\"
-copy "Fronter\Resources\converter.ico" "Release\"
+copy "wx\3.1.3\lib\vc14x_x64_dll\wxbase313u_vc14x_x64.dll" "..\Release\"
+copy "wx\3.1.3\lib\vc14x_x64_dll\wxmsw313u_core_vc14x_x64.dll" "..\Release\"
+copy "Fronter\Resources\converter.ico" "..\Release\"
 
 rem Create Configuration
 rem del "Release\Configuration" /Q
