5a7f6fdf9c0801a4207b20eb6bee7935d9987277
Ivo Elezovic <ivo@elezovic.su>
2020-04-30 22:59:11 +0200

    Fix for log overload freeze

--- Copy_Files.bat
+++ Copy_Files.bat
@@ -3,6 +3,9 @@ rem Copy required DLLs
 copy "wx\3.1.3\lib\vc14x_x64_dll\wxbase313u_vc14x_x64.dll" "..\Release\"
 copy "wx\3.1.3\lib\vc14x_x64_dll\wxmsw313u_core_vc14x_x64.dll" "..\Release\"
 copy "Fronter\Resources\converter.ico" "..\Release\"
+copy "Fronter\Resources\msvcp140_codecvt_ids.dll" "..\Release\"
+copy "Fronter\Resources\vcruntime140_1.dll" "..\Release\"
+copy "Fronter\Resources\VC_redist.x64.exe" "..\Release\"
 
 rem Create Configuration
 rem del "Release\Configuration" /Q
