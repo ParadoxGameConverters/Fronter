2c0fa7fc29b3b559f962f9f7f42a83f49f0f3680
Ivo Elezovic <ivo@elezovic.su>
2020-05-04 18:39:57 +0200

    prep for releaase

--- Copy_Files.bat
+++ Copy_Files.bat
@@ -1,21 +1,21 @@
 echo on
 rem Copy required DLLs
-copy "wx\3.1.3\lib\vc14x_x64_dll\wxbase313u_vc14x_x64.dll" "Release\"
-copy "wx\3.1.3\lib\vc14x_x64_dll\wxmsw313u_core_vc14x_x64.dll" "Release\"
-copy "Fronter\Resources\converter.ico" "Release\"
-copy "Fronter\Resources\msvcp140_codecvt_ids.dll" "Release\"
-copy "Fronter\Resources\vcruntime140_1.dll" "Release\"
-copy "Fronter\Resources\VC_redist.x64.exe" "Release\"
+copy "wx\3.1.3\lib\vc14x_x64_dll\wxbase313u_vc14x_x64.dll" "..\Release\"
+copy "wx\3.1.3\lib\vc14x_x64_dll\wxmsw313u_core_vc14x_x64.dll" "..\Release\"
+copy "Fronter\Resources\converter.ico" "..\Release\"
+copy "Fronter\Resources\msvcp140_codecvt_ids.dll" "..\Release\"
+copy "Fronter\Resources\vcruntime140_1.dll" "..\Release\"
+copy "Fronter\Resources\VC_redist.x64.exe" "..\Release\"
 
 rem Create Configuration
-del "Release\Configuration" /Q
-rmdir "Release\Configuration" /S /Q
-xcopy "Fronter\Configuration" "Release\Configuration" /Y /E /I
-mkdir "Release\Configuration"
-copy "Fronter\Resources\converter_languages.yml" "Release\Configuration\"
-copy "Fronter\Resources\converter_l_french.yml" "Release\Configuration\"
-copy "Fronter\Resources\converter_l_english.yml" "Release\Configuration\"
+rem del "Release\Configuration" /Q
+rem rmdir "Release\Configuration" /S /Q
+rem xcopy "Fronter\Configuration" "Release\Configuration" /Y /E /I
+mkdir "..\Release\Configuration"
+copy "Fronter\Resources\converter_languages.yml" "..\Release\Configuration\"
+copy "Fronter\Resources\converter_l_french.yml" "..\Release\Configuration\"
+copy "Fronter\Resources\converter_l_english.yml" "..\Release\Configuration\"
 
-del "Release\CK2ToEU4" /Q
-rmdir "Release\CK2ToEU4" /S /Q
-xcopy "..\CK2ToEU4\Release\CK2ToEU4" "Release\CK2ToEU4" /Y /E /I
+rem del "Release\CK2ToEU4" /Q
+rem rmdir "Release\CK2ToEU4" /S /Q
+rem xcopy "..\CK2ToEU4\Release\CK2ToEU4" "Release\CK2ToEU4" /Y /E /I
