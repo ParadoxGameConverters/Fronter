9be01292ca2b7b77449c2b1d1f3b27eccf35b87b
IhateTrains <IhateTrains@users.noreply.github.com>
2020-11-14 22:07:20 +0100

    Retargetted to c++20 (#54)

--- build_linux.sh
+++ build_linux.sh
@@ -2,6 +2,6 @@
 cd Fronter &&
 rm -rf build &&
 rm -rf ../Release &&
-cmake -H. -Bbuild -DCMAKE_C_COMPILER=/usr/bin/gcc-9 -DCMAKE_CXX_COMPILER=/usr/bin/g++-9 &&
+cmake -H. -Bbuild -DCMAKE_C_COMPILER=/usr/bin/gcc-10 -DCMAKE_CXX_COMPILER=/usr/bin/g++-10 &&
 cmake --build build -- -j3 &&
 cd ..
