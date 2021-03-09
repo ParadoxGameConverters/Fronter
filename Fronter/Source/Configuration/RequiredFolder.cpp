f90a43cccf517c803afbe97b88de9dd43daeec2f
IlikeTrains <sewerynpresnal@gmail.com>
2021-03-09 23:24:07 +0100

    commonItems update, Configuration.cpp tweaks

--- Fronter/Source/Configuration/RequiredFolder.cpp
+++ Fronter/Source/Configuration/RequiredFolder.cpp
@@ -1,6 +1,7 @@
 #include "RequiredFolder.h"
 #include "Log.h"
 #include "ParserHelpers.h"
+#include "CommonRegexes.h"
 
 RequiredFolder::RequiredFolder(std::istream& theStream)
 {
@@ -11,35 +12,35 @@ RequiredFolder::RequiredFolder(std::istream& theStream)
 
 void RequiredFolder::registerKeys()
 {
-	registerKeyword("name", [this](const std::string& unused, std::istream& theStream) {
+	registerKeyword("name", [this](std::istream& theStream) {
 		const commonItems::singleString nameStr(theStream);
 		name = nameStr.getString();
 	});
-	registerKeyword("tooltip", [this](const std::string& unused, std::istream& theStream) {
+	registerKeyword("tooltip", [this](std::istream& theStream) {
 		const commonItems::singleString tooltipStr(theStream);
 		tooltip = tooltipStr.getString();
 	});
-	registerKeyword("displayName", [this](const std::string& unused, std::istream& theStream) {
+	registerKeyword("displayName", [this](std::istream& theStream) {
 		const commonItems::singleString nameStr(theStream);
 		displayName = nameStr.getString();
 	});
-	registerKeyword("mandatory", [this](const std::string& unused, std::istream& theStream) {
+	registerKeyword("mandatory", [this](std::istream& theStream) {
 		const commonItems::singleString mandatoryStr(theStream);
 		mandatory = mandatoryStr.getString() == "true";
 	});
-	registerKeyword("outputtable", [this](const std::string& unused, std::istream& theStream) {
+	registerKeyword("outputtable", [this](std::istream& theStream) {
 		const commonItems::singleString outputtableStr(theStream);
 		outputtable = outputtableStr.getString() == "true";
 	});
-	registerKeyword("searchPathType", [this](const std::string& unused, std::istream& theStream) {
+	registerKeyword("searchPathType", [this](std::istream& theStream) {
 		const commonItems::singleString pathStr(theStream);
 		searchPathType = pathStr.getString();
 	});
-	registerKeyword("searchPathID", [this](const std::string& unused, std::istream& theStream) {
+	registerKeyword("searchPathID", [this](std::istream& theStream) {
 		const commonItems::singleString pathStr(theStream);
 		searchPathID = pathStr.getString();
 	});
-	registerKeyword("searchPath", [this](const std::string& unused, std::istream& theStream) {
+	registerKeyword("searchPath", [this](std::istream& theStream) {
 		const commonItems::singleString pathStr(theStream);
 		searchPath = pathStr.getString();
 	});
