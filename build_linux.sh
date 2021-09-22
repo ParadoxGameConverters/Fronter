2753295b3e3b1aacdeee4296962f09e93521e9a7
Ivo Elezović <ivo@element.hr>
2021-09-23 01:54:51 +0200

    up compiler corecount

--- build_linux.sh
+++ build_linux.sh
@@ -3,5 +3,5 @@ cd Fronter &&
 rm -rf build &&
 rm -rf ../Release &&
 cmake -H. -Bbuild -DCMAKE_C_COMPILER=/usr/bin/gcc-11 -DCMAKE_CXX_COMPILER=/usr/bin/g++-11 &&
-cmake --build build -- -j3 &&
+cmake --build build -- -j40 &&
 cd ..
