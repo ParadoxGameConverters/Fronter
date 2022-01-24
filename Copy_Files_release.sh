8db898e8d2d7d2de0dbeca03ff6b9600695f4c48
Ivo Elezovic <ivo@elezovic.su>
2022-01-24 13:02:48 +0100

    General cross-converter FAQ

--- Copy_Files_release.sh
+++ Copy_Files_release.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 # Copy required resources
 cp Resources/converter.ico ../Release/
+cp Resources/GeneralFAQ-READ.ME.FIRST.txt ../Release/
 
 rm -rf ../Release/Configuration
 mkdir ../Release/Configuration
