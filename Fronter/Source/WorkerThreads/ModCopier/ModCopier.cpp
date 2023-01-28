c955df05029d4427c7b66fed314aabc219a8fb85
Ivo Elezović <ivo@element.hr>
2023-01-28 20:15:30 +0100

    reshuffle

--- Fronter/Source/WorkerThreads/ModCopier/ModCopier.cpp
+++ Fronter/Source/WorkerThreads/ModCopier/ModCopier.cpp
@@ -1,11 +1,43 @@
 #include "ModCopier.h"
-#include "Log.h"
-#include <filesystem>
-namespace fs = std::filesystem;
 #include "CommonFunctions.h"
+#include "Log.h"
 #include "OSCompatibilityLayer.h"
+#include <chrono>
+#include <filesystem>
+#include <random>
+namespace fs = std::filesystem;
 
 wxDEFINE_EVENT(wxEVT_COPIERDONE, wxCommandEvent);
+
+namespace
+{
+unsigned char random_char()
+{
+	std::random_device rd;
+	std::mt19937 gen(rd());
+	std::uniform_int_distribution<> dis(0, 255);
+	return static_cast<unsigned char>(dis(gen));
+}
+
+std::string generate_hex(const unsigned int len)
+{
+	std::stringstream ss;
+	for (unsigned int i = 0; i < len; i++)
+	{
+		const auto rc = random_char();
+		std::stringstream hexStream;
+		hexStream << std::hex << static_cast<int>(rc);
+		auto hex = hexStream.str();
+		ss << (hex.length() < 2 ? '0' + hex : hex);
+	}
+	return ss.str();
+}
+
+std::string generate_uuid()
+{
+	return generate_hex(4) + "-" + generate_hex(2) + "-" + generate_hex(2) + "-" + generate_hex(2) + "-" + generate_hex(6);
+}
+} // namespace
 
 void* ModCopier::Entry()
 {
@@ -149,7 +181,132 @@ void* ModCopier::Entry()
 		return nullptr;
 	}
 	Log(LogLevel::Notice) << "Mod successfully copied to: " << destinationFolder + "/" + targetName;
+
+	CreatePlayset(destinationFolder, targetName, destinationFolder + "/" + targetName);
+
 	evt.SetInt(1);
 	m_pParent->AddPendingEvent(evt);
 	return nullptr;
+}
+
+void ModCopier::CreatePlayset(const std::string& destModFolder, const std::string& targetName, const std::string& destModFolderPath)
+{
+	const auto gameDocsDirectory = destModFolder + "/..";
+	if (!commonItems::DoesFolderExist(gameDocsDirectory))
+	{
+		Log(LogLevel::Warning) << "Couldn't resolve parent folder of " << destModFolder;
+		return;
+	}
+
+	const auto latestDbFilePath = GetLastUpdatedLauncherDbPath(gameDocsDirectory);
+	if (latestDbFilePath.empty())
+	{
+		Log(LogLevel::Info) << "Launcher's database not found or this game doesn't have a launcher.";
+		Log(LogLevel::Notice) << "All done. Have fun! -- Paradox Game Converters Team";
+		return;
+	}
+
+	Log(LogLevel::Notice) << "Enabling Playset, please hold.";
+
+	try
+	{
+		SQLite::Database db(latestDbFilePath, SQLite::OPEN_READWRITE);
+		std::string playsetName = configuration->getName() + ": " + targetName;
+		auto unixTimeMilliSeconds = std::chrono::system_clock::now().time_since_epoch().count();
+
+		SQLite::Statement query(db, "SELECT COUNT(*) FROM playsets WHERE name = ?");
+		query.bind(1, playsetName);
+		query.executeStep();
+		if (query.getColumn(0).getInt() > 0)
+		{
+			if (configuration->getOverwritePlayset())
+			{
+				DeactivateCurrentPlayset(db);
+
+				Log(LogLevel::Notice) << "Reactivating existing playset " << playsetName << ".";
+				SQLite::Statement query2(db, "UPDATE playsets SET isActive=true, updatedOn = ? WHERE name = ?");
+				query2.bind(1, unixTimeMilliSeconds);
+				query2.bind(2, playsetName);
+				query2.exec();
+				Log(LogLevel::Notice) << "Have fun! -- Paradox Game Converters Team";
+			}
+			else
+			{
+				Log(LogLevel::Notice) << "Playset " + playsetName + " already exists, select it and play. Have fun!  -- Paradox Game Converters Team";
+			}
+		}
+		else
+		{
+			DeactivateCurrentPlayset(db);
+			const auto playsetID = generate_uuid();
+			SQLite::Statement query2(db,
+				 "INSERT INTO playsets(id, name, isActive, isRemoved, hasNotApprovedChanges, createdOn) "
+				 "VALUES(?, ?, true, false, false, ?)");
+			query2.bind(1, playsetID);
+			query2.bind(2, playsetName);
+			query2.bind(3, unixTimeMilliSeconds);
+			query2.exec();
+
+			auto gameRegistryId = "mod/" + targetName + ".mod";
+			auto modId = AddModToDb(db, targetName, gameRegistryId, destModFolder);
+			AddModToPlayset(db, modId, playsetID);
+			Log(LogLevel::Notice) << "Playset " + playsetName + " created, select it and play. Have fun! -- Paradox Game Converters Team";
+		}
+	}
+	catch (std::exception& e)
+	{
+		Log(LogLevel::Error) << e.what();
+		Log(LogLevel::Notice) << "Adding mod/playset to the launcher failed. You need to do it yourself.";
+	}
+}
+
+std::string ModCopier::GetLastUpdatedLauncherDbPath(const std::string& gameDocsDirectory) const
+{
+	const std::set<std::string> possibleDbFileNames = {"launcher-v2.sqlite", "launcher-v2_openbeta.sqlite"};
+	fs::file_time_type lastAccess;
+	std::string actualName;
+	for (const auto& name: possibleDbFileNames)
+	{
+		auto path = std::filesystem::path(gameDocsDirectory + "/" + name);
+		if (!commonItems::DoesFileExist(gameDocsDirectory + "/" + name))
+			continue;
+		if (lastAccess < last_write_time(path))
+		{
+			lastAccess = last_write_time(path);
+			actualName = name;
+		}
+	}
+	if (actualName.empty())
+		return actualName;
+	return gameDocsDirectory + "/" + actualName;
+}
+
+void ModCopier::DeactivateCurrentPlayset(SQLite::Database& db)
+{
+	Log(LogLevel::Debug) << "Deactivating currently active playset.";
+	SQLite::Statement query(db, "UPDATE playsets SET isActive=false");
+	query.exec();
+}
+
+std::string ModCopier::AddModToDb(SQLite::Database& db, const std::string& modName, const std::string& gameRegistryId, const std::string& dirPath)
+{
+	auto modID = generate_uuid();
+	SQLite::Statement query(db,
+		 "INSERT INTO mods(id, status, source, version, gameRegistryId, name, dirPath) "
+		 "VALUES(?, 'ready_to_play', 'local', 1, ?, ?, ?)");
+	query.bind(1, modID);
+	query.bind(2, gameRegistryId);
+	query.bind(3, modName);
+	query.bind(4, dirPath);
+	query.exec();
+
+	return modID;
+}
+
+void ModCopier::AddModToPlayset(SQLite::Database& db, const std::string& modID, const std::string& playsetID)
+{
+	SQLite::Statement query(db, "INSERT INTO playsets_mods(playsetId, modId) VALUES(?, ?)");
+	query.bind(1, playsetID);
+	query.bind(2, modID);
+	query.exec();
 }
--- Fronter/Source/WorkerThreads/ModCopier/ModCopier.h
+++ Fronter/Source/WorkerThreads/ModCopier/ModCopier.h
@@ -3,7 +3,7 @@
 #ifndef WX_PRECOMP
 #include <wx/wx.h>
 #endif
-
+#include "SQLiteCpp/SQLiteCpp.h"
 #include "../../Configuration/Configuration.h"
 #include <wx/thread.h>
 
@@ -17,6 +17,12 @@ class ModCopier final: public wxThread
 
   private:
 	void* Entry() override;
+	void CreatePlayset(const std::string& destModFolder, const std::string& targetName, const std::string& destModFolderPath);
+	[[nodiscard]] std::string GetLastUpdatedLauncherDbPath(const std::string& gameDocsDirectory) const;
+	void DeactivateCurrentPlayset(SQLite::Database& db);
+	std::string AddModToDb(SQLite::Database& db, const std::string& modName, const std::string& gameRegistryId, const std::string& dirPath);
+	void AddModToPlayset(SQLite::Database& db, const std::string& modID, const std::string& playsetID);
+
 	std::shared_ptr<Configuration> configuration;
 
   protected:
