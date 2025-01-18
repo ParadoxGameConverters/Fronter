3f07535341962dd6f6b06c46ce639a54c5727d56
Idhrendur <idhrendur@hotmail.com>
2025-01-18 11:46:11 -0800

    Even more workflow fixes

--- build_linux.sh
+++ build_linux.sh
@@ -3,5 +3,5 @@ export CC=/usr/bin/gcc-11 &&
 export CXX=/usr/bin/g++-11 &&
 rm -rf build &&
 rm -rf Release &&
-cmake -H. --preset x64-coverage-linux -DCMAKE_C_COMPILER=/usr/bin/gcc-11 -DCMAKE_CXX_COMPILER=/usr/bin/g++-11 &&
-cmake --build --preset build-x64-coverage-linux --target ConverterFrontend -- -j40
+cmake -H. --preset x64-release-linux -DCMAKE_C_COMPILER=/usr/bin/gcc-11 -DCMAKE_CXX_COMPILER=/usr/bin/g++-11 &&
+cmake --build --preset build-x64-release-linux --target ConverterFrontend -- -j40
