ea15798c70f5f3c22f75acc8e00b3f29a5a0dd28
Idhrendur <idhrendur@hotmail.com>
2025-01-18 10:09:21 -0800

    Update build script

--- build_linux.sh
+++ build_linux.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 export CC=/usr/bin/gcc-11 &&
 export CXX=/usr/bin/g++-11 &&
-cd Fronter &&
 rm -rf build &&
-rm -rf ../Release &&
+rm -rf Release &&
 cmake -H. -Bbuild -DCMAKE_C_COMPILER=/usr/bin/gcc-11 -DCMAKE_CXX_COMPILER=/usr/bin/g++-11 &&
-cmake --build build -- -j40 &&
-cd ..
+cmake --build build -- -j40
